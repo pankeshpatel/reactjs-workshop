5c1c4ca4d7edae0eca536771e935b63b8593ecbe
Pankesh Patel <pankeshpatel@MacBook-Pro.local>
2020-12-31 12:28:07 +0530

    added fat arrow function

--- README.md
+++ README.md
@@ -39,6 +39,6 @@ This repository contains a step-by-step tutorial and code snippet to learn react
 10. React Project into Components: `#19` (code: `tut7`)
 11. ES6 Modules Import Export : `#20`, `#21` (code: `tut8`)
 12. React JS Props (Netflix App): `#22` , `#23` (code: `tut9`)
-
+13. ReactJS JavaScript Array Map Method :    (Code: `tut10`)
 
   
--- thapa/tut9/src/index.js
+++ thapa/tut9/src/index.js
@@ -13,33 +13,83 @@ import Cdata from './Cdata';
 // imgsrc is a custome "properties" (Props) of Card
 // 
 
+// ReactDOM render method...
+
+// ReactDOM.render(
+//   <>
+//   <h1 className="heading">List of top Series in 2020</h1>
+//   <Card 
+//     imgsrc={Cdata[0].isrc}
+//     title = {Cdata[0].sname}
+//     sname = {Cdata[0].span}
+//     link={Cdata[0].link} 
+
+//   />
+
+//   <Card 
+//     imgsrc={Cdata[1].isrc}
+//     title = {Cdata[1].sname}
+//     sname = {Cdata[1].span}
+//     link={Cdata[1].link}     
+//   />
+
+// <Card 
+//     imgsrc={Cdata[2].isrc}
+//     title = {Cdata[2].sname}
+//     sname = {Cdata[2].span}
+//     link={Cdata[2].link}     
+//   />  
+//   </>,
+//   document.getElementById('root')
+// );
+
+// map function....
+
+function ncard(val){
+   return (
+    <Card 
+    imgsrc={val.isrc}
+    title = {val.sname}
+    sname = {val.span}
+    link={val.link} 
+    />
+   );
+}
+
+// fat arrow function
+
+
 ReactDOM.render(
   <>
   <h1 className="heading">List of top Series in 2020</h1>
-  <Card 
-    imgsrc={Cdata[0].isrc}
-    title = {Cdata[0].sname}
-    sname = {Cdata[0].span}
-    link={Cdata[0].link} 
-
-  />
-
-  <Card 
-    imgsrc={Cdata[1].isrc}
-    title = {Cdata[1].sname}
-    sname = {Cdata[1].span}
-    link={Cdata[1].link}     
-  />
-
-<Card 
-    imgsrc={Cdata[2].isrc}
-    title = {Cdata[2].sname}
-    sname = {Cdata[2].span}
-    link={Cdata[2].link}     
-  />  
+  {Cdata.map(ncard)}  
   </>,
   document.getElementById('root')
 );
+
+// fat arrow function
+// function myname(a, b){
+//  return a + b;  
+// }
+
+// // Fat arrow function
+// const myname = (a,b) => {
+//   return a + b;
+// }
+
+// Second method to call the function
+
+// {Cdata.map(function ncard(val){
+//   return (
+//    <Card 
+//    imgsrc={val.isrc}
+//    title = {val.sname}
+//    sname = {val.span}
+//    link={val.link} 
+//    />
+//   );
+// })}  
+
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
