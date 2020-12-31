d2a3405bda919bd02d0d0381063ad8015bc580d1
Pankesh Patel <pankeshpatel@MacBook-Pro.local>
2020-12-31 17:24:10 +0530

    added reactjs event

--- thapa/tut12/src/App.js
+++ thapa/tut12/src/App.js
@@ -1,24 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react'
+
 
 function App() {
+  const [name, setName] = useState("");
+  const [fullName, setFullName] = useState("");
+   
+  function inputEvent(event){
+    //console.log(event.target.value);
+    setName(event.target.value);
+  }
+
+  function onSubmit(){
+    setFullName(name);
+  }
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div>
+      <h1> Hello {fullName} </h1>
+      <input type="text" 
+      placeholder ="Enter your name" 
+      onChange = {inputEvent} />
+      <button onClick={onSubmit}>Click Me</button>
+      </div>
+    </>
   );
 }
 
